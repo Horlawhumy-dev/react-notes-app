c4daa879d99591adb6eaacc380968139766c498d
Arafat Olayiwola <harof.dev@gmail.com>
2021-05-19 16:31:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NoteList from './components/NoteList'
+const App = () => {
+   return (
+     <div className="container">
+        <NoteList />
+     
+     </div>
+   )
+}
+
+
+export default App;
